aa1dcef97310c99cd2c23cc6785f3b491aadf90a
James Pearce <github@tripleodeon.com>
2025-04-18 11:29:16 -0930

    First update

--- data.js
+++ data.js
@@ -1,41 +1,112 @@
 // @ts-check
-import { LandscapeSchema } from '@localfirstfm/landscape-schema'
+import { LandscapeSchema } from "@localfirstfm/landscape-schema";
 
 export const data = LandscapeSchema.make({
-	Version: 1,
-	Id: 'supersync',
-	Name: 'SuperSync',
-	Description: 'A cool way to sync data',
-	Website: 'https://supersync.cool',
-	Deployment: ['Self-hosted'],
-	License: 'MIT',
-	AppTarget: {
-		LanguageSDK: { data: ['typescript'] },
-	},
-	Networking: {
-		Topology: { data: 'Client-Server' },
-	},
-	ServerSideData: {
-		PersistenceMechanism: { data: ['N/A'] },
-		DataModelParadigm: { data: 'Relational' },
-	},
-	ClientSideData: {
-		QueryAPI: { data: ['Async'] },
-		PersistenceMechanism: { data: ['IndexedDB', 'OPFS'] },
-		PersistenceFeatures: { data: 'Indexes' },
-		DataModel: { data: 'Document' },
-		OfflineReads: { data: 'Full Support' },
-		OfflineWrites: { data: 'Local conflict resolution' },
-		DataSize: { data: 'Up to 5-10 MB per document' },
-	},
-	SynchronizationStrategy: {
-		FullOrPartialReplication: { data: ['Full Replication'] },
-		ConflictHandling: { data: 'Automatic via CRDT' },
-		WhereResolutionOccurs: { data: 'Client' },
-		WhatGetsSynced: {
-			data: {
-				ClientToClient: 'Ops',
-			},
-		},
-	},
-})
+  Version: 1,
+  Id: "tinybase",
+  Name: "TinyBase",
+  Description: "A reactive data store & sync engine.",
+  Website: "https://tinybase.org",
+  GitHub: "https://github.com/tinyplex/tinybase",
+  Logo: {
+    Light: "https://tinybase.org/favicon.svg",
+    Dark: "https://tinybase.org/favicon.svg",
+  },
+  MaturityLevel: "Mature",
+  InitialReleaseDate: new Date("2022-01-17"),
+  Deployment: ["Self-hosted"],
+  License: "MIT",
+  NotableAdopters: [
+    { Name: "Athena Crisis", URL: "https://athenacrisis.com" },
+    { Name: "TinyHub", URL: "https://tinyhub.org" },
+  ],
+  AppTarget: {
+    LanguageSDK: { data: ["TypeScript", "JavaScript"] },
+    Platform: {
+      data: ["Browser", "Node", "React Native", "Deno", "Cloudflare Workers"],
+    },
+    FrameworkIntegrations: {
+      data: ["React", "React Native"],
+    },
+    ClientBundleSize: {
+      data: "5.3kB - 11.5kB",
+      comment: "The clue is in the name!",
+    },
+  },
+  Networking: {
+    Protocol: {
+      data: ["WebSockets", "HTTP"],
+    },
+    Topology: {
+      data: "P2P via Relay Servers",
+      comment:
+        "Client-server is also made possible by an API that lets the server participate as a peer.",
+    },
+  },
+  ServerSideData: {
+    PersistenceMechanism: {
+      data: [
+        "Cloudflare Durable Object Storage",
+        "SQLite",
+        "Postgres",
+        "Custom",
+      ],
+    },
+    DataSize: {
+      data: "Up to 5-10 MB per store",
+      comment:
+        "The server can also use a database like SQLite or Postgres for larger data.",
+    },
+    DataModelParadigm: {
+      data: "Relational",
+      comment: "Both relational and key-value data can be stored and synced.",
+    },
+  },
+  ClientSideData: {
+    QueryAPI: { data: ["Sync", "Reactive queries"] },
+    LocalRefreshLatency: { data: "~1ms" },
+    PersistenceMechanism: {
+      data: [
+        "SQLite",
+        "IndexedDB",
+        "PGLite via OPFS",
+        "SQLite",
+        "Yjs",
+        "Custom",
+      ],
+    },
+    PersistenceFeatures: { data: "Indexes" },
+    DataModel: { data: "Document" },
+    OfflineReads: { data: "Full Support" },
+    OfflineWrites: { data: "Local conflict resolution" },
+    DataSize: { data: "Up to 5-10 MB per store" },
+  },
+  SynchronizationStrategy: {
+    FullOrPartialReplication: { data: ["Full Replication"] },
+    ConflictHandling: { data: "Automatic via CRDT" },
+    WhereResolutionOccurs: {
+      data: "Client",
+      comment: "Server can also resolve conflicts.",
+    },
+    WhatGetsSynced: {
+      data: {
+        ClientToServer: "Cell- or value-level changes",
+        ServerToClient: "Cell- or value-level changes",
+        ClientToClient: "Cell- or value-level changes",
+      },
+    },
+  },
+  UIRelated: {
+    Components: {
+      data: ["React", "ReactDom"],
+    },
+  },
+  DevelopmentWorkflowsDX: {
+    DebuggingTools: {
+      data: ["Data Inspector"],
+    },
+    TypeSupport: {
+      data: "TypeScript, inferred from Schemas",
+    },
+  },
+});
--- details.md
+++ details.md
@@ -1,15 +1,43 @@
-# SuperSync
+# TinyBase
 
-SuperSync is a cool way to sync your data.
+A <em>reactive</emdata store <span>& <em>sync</emengine</span>.
 
-## Features
+<a class='start' href='/guides/the-basics/getting-started/'>Get started</a>
 
-- **SuperSync**: Sync your data with ease.
-- **Local-first**: Your data is stored locally, ensuring privacy and security.
-- **Cross-platform**: Works on all major platforms.
-- **Open-source**: The code is available for anyone to use and contribute to.
-- **Community-driven**: Join our community and help us improve the project.
+<a href='/demos/'>Try the demos</a>
 
-## More info
+<a href='/api/store/interfaces/store/store/'>Read the docs</a>
 
-...
+---
+
+## It's _Reactive_
+
+TinyBase lets you listen to changes made to any part of your data. This means
+your app will be fast, since you only spend rendering cycles on things that
+change. The optional bindings to React and pre-built components let you easily
+build fully reactive UIs on top of TinyBase. You even get a built-in undo
+stack, and developer tools!
+
+## It's _Database-Like_
+
+Consumer app? Enterprise app? Or even a game? Model key-value data and tabular
+data with optional typed schematization, whatever its data structures. There
+are built-in indexing, metric aggregation, and tabular relationships APIs -
+and a powerful query engine to select, join, filter, and group data
+(reactively!) without SQL.
+
+## It _Synchronizes_
+
+TinyBase has native CRDT support, meaning that you can deterministically
+synchronize and merge data across multiple sources, clients, and servers. And
+although TinyBase is an in-memory data store, you can easily persist your data
+to file, browser storage /persister-browser, IndexedDB, SQLite or PostgreSQL
+databases, and more.
+
+## It's Built For A _Local-First_ World
+
+TinyBase works anywhere that JavaScript does, but it's especially great for
+local-first apps: where data is stored locally on the user's device and that
+can be run offline. It's tiny by name, tiny by nature: just 5.3kB - 11.5kB and
+with no dependencies - yet 100% tested, fully documented, and of course, open
+source!
